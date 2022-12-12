edaabae5ea06584ba5cb6fed8d7ee4613ee7395f
Rob Carruthers <rob-carruthers@users.noreply.github.com>
2022-12-12 10:23:24 +0000

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    output = "";
+    
+    if ( string.length < 1 ) {
+        return output;
+    }
 
+    for (let i = string.length; i > 0; i--) {
+        output += string.charAt(i - 1);
+    }
+
+    return output;
 };
 
 // Do not edit below this line
